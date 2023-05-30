3ef1163efe53bf98826a213176cacf6e74bdbbe6
Adam <thomasadam83@hotmail.com>
2023-05-30 14:11:09 -0400

    Finished rating sample

--- src/components/MuiRating.tsx
+++ src/components/MuiRating.tsx
@@ -1,11 +1,26 @@
 import {Stack, Rating} from "@mui/material";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import {useState} from "react";
 
 export const MuiRating = () => {
-  const [value, setValue] = useState<number | null>(null);
+  const [value, setValue] = useState<number | null>(3);
+  console.log(value);
+  const handleChange = (
+    _event: React.ChangeEvent<{}>,
+    newValue: number | null
+  ) => {
+    setValue(newValue);
+  };
   return (
     <Stack spacing="2">
-      <Rating value={value} onChange={handleChange} />
+      <Rating
+        value={value}
+        onChange={handleChange}
+        precision={0.5}
+        icon={<FavoriteIcon />}
+        emptyIcon={<FavoriteBorderIcon />}
+      />
     </Stack>
   );
 };
